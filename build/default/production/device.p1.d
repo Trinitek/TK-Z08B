a4ad05ffb01979d42dd79943a2daf137e24d054f
Blake Burgess <trinitek@live.com>
2014-11-10 19:30:48 -0500

    Add .gitignore

--- build/default/production/device.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/device.d  \
- build/default/production/device.p1:  \
- device.c  \
-memory.h 
--- build/default/production/interrupt.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/interrupt.d  \
- build/default/production/interrupt.p1:  \
- interrupt.c  \
-ports.h 
