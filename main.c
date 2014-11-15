fbc257b3a8e95f52ecaa084319ae70c7f078f9bc
Blake Burgess <trinitek@live.com>
2014-11-14 23:52:51 -0500

    Remove unused function in main file

--- main.c
+++ main.c
@@ -21,9 +21,6 @@ void main(void) {
         "Controller: 8 MHz | CPU: 2 MHz\n"
         "Serial: 2400 baud\n"
         "\n";
-    //uint8_t greet[] = "Hello world! ";
-    //uint8_t halted[] = "Halted. ";
-    //uint8_t rxChar;
 
     uint8_t addrString[9];
     addrString[8] = 0;
@@ -33,23 +30,8 @@ void main(void) {
     sendString(welcome);
 
     while(true) {
-        // Convert address bus signals to writable binary string
-        /*if (a0Input) addrString[7] = '1'; else addrString[7] = '0';
-        if (a1Input) addrString[6] = '1'; else addrString[6] = '0';
-        if (a2Input) addrString[5] = '1'; else addrString[5] = '0';
-        if (a3Input) addrString[4] = '1'; else addrString[4] = '0';
-        if (a4Input) addrString[3] = '1'; else addrString[3] = '0';
-        if (a5Input) addrString[2] = '1'; else addrString[2] = '0';
-        if (a6Input) addrString[1] = '1'; else addrString[1] = '0';
-        if (a7Input) addrString[0] = '1'; else addrString[0] = '0';
-        sendString(addrString);*/
         binToString(addressBusInput, addrString);
         sendString(addrString);
         sendString(linefeed);
     }
-}
-
-bool checkForMemoryRead(void) {
-    if (cpuClockOutput == 1 && 0); // TODO
-    return false;
 }
