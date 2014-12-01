c6761f0c55424c3fedcbd797ef5f095fd27b4039
Blake Burgess <trinitek@live.com>
2014-12-01 00:42:54 -0500

    Remove timer device port, reassign serial

--- device.c
+++ device.c
@@ -7,23 +7,17 @@
 // input to CPU
     // interrupt code
 #define interruptIn         0
-    // timer
-#define timerConfigIn       1
-#define timerCountIn        4
     // RS-232
-#define serialConfigIn      5
-#define serialByteIn        6
+#define serialConfigIn      1
+#define serialByteIn        3
     // EEPROM
 #define eepromAddrIn        7
 #define eepromDataIn        8
 
 // output from CPU
-    // timer
-#define timerConfigOut      1
-#define timerCountOut       4
     // RS-232
-#define serialConfigOut     5
-#define serialByteOut       6
+#define serialConfigOut     1
+#define serialByteOut       2
     // EEPROM
 #define eepromAddrOut       7
 #define eepromDataOut       8
